ad1eee00503e55b8c86e5ad1b18415186e8c6209
DileepKGowda <148583858+DileepKGowda@users.noreply.github.com>
2023-12-08 12:05:03 +0530

    Add files via upload

---
+++ js/jquery.countdown.hr.js
@@ -0,0 +1,29 @@
+// Set the date to countdown to (YYYY, MM (0-indexed), DD, HH, MM, SS)
+const countdownDate = new Date('2024-01-01T00:00:00').getTime();
+
+// Update the countdown every second
+const countdown = setInterval(function() {
+  const now = new Date().getTime();
+  const distance = countdownDate - now;
+
+  // Calculate days, hours, minutes, and seconds
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the countdown
+  const countdownElement = document.getElementById('countdown');
+  countdownElement.innerHTML = `
+    <div>${days} Days</div>
+    <div>${hours} Hours</div>
+    <div>${minutes} Minutes</div>
+    <div>${seconds} Seconds</div>
+  `;
+
+  // If the countdown is finished, display a message
+  if (distance < 0) {
+    clearInterval(countdown);
+    countdownElement.innerHTML = 'Countdown is over!';
+  }
+}, 1000);
